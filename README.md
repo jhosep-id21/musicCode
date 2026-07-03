7b4c3981102e22d3d779e2b08c102f7f6ae1077d
jhosep_id21 <jhosepaled@gmail.com>
2026-07-03 14:40:56 -0500

    Regresar carpeta al estado del commit 3838385

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # musicCode
-=======
-# jhosep-id21.github.io
->>>>>>> aa7a2d96f90710331b320c594a5c5883e2087ead
--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
@@ -9,15 +8,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-=======
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleBtn = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-
-    if (toggleBtn && navLinks) {
-        toggleBtn.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
-    }
-});
->>>>>>> aa7a2d96f90710331b320c594a5c5883e2087ead
